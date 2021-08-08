bcfd1b2f6f8363bf40c57395b66e45f695dd17e8
Advay Sharma <sharmaadvay0@gmail.com>
2021-08-08 14:33:10 -0700

    parsing data fron classData.json

--- backend/api/generateSchedules.py
+++ backend/api/generateSchedules.py
@@ -3,14 +3,41 @@ import json
 
 def generateAllSchedules(classes):
     classData = getClassData()
-    for className in classes:
-        data = classData[className]
-    return "works"
+    courseList = classData["courseList"]
+    classesSet = set(classes)
+    classesInfo = {}
+    for course in courseList:
+        className = (
+            course["subjectCode"].replace(" ", "")
+            + " "
+            + course["courseCode"].replace(" ", "")
+        )
+        if className in classesSet:
+            classesInfo.update(
+                {
+                    className: {
+                        "lectures": [],
+                        "sections": [],
+                    }
+                }
+            )
+            for i in range(len(course["sections"])):
+                sectionCode = course["sections"][i]["sectionCode"]
+                sectionTimes = course["sections"][i]["recurringMeetings"]
+                if sectionCode[1:] != "00":
+                    classesInfo[className]["sections"].append(
+                        {sectionCode: sectionTimes}
+                    )
+                else:
+                    classesInfo[className]["lectures"].append(
+                        {sectionCode: sectionTimes}
+                    )
+    return classesInfo
 
 
 def getClassData():
     file = open(
-        "api/fakeClassData.json",
+        "api/classData.json",
         "r",
     )
     return json.load(file)
